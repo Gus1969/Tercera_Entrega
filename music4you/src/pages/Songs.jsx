99e05d8c7c71db75be428f0a03c2d8c8cd6852be
gustavo <gustavo.ubalperez@gmail.com>
2021-12-15 13:36:21 -0300

    adding json

---
+++ music4you/src/pages/Songs.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import canciones from './canciones.json';
+
+
+const Songs = () => {
+    return (
+        <div>
+            <div className="posting">
+                {canciones.map(cancion => {
+                    return(
+                        
+                    )
+                }
+            </div>
+            
+        </div>
+    )
+}
+
+export default Songs
