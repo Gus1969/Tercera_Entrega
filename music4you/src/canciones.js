a4e104d5c81c5036e2327143a5c774f77b20e39f
gustavo <gustavo.ubalperez@gmail.com>
2021-12-17 17:22:31 -0300

    layout

--- music4you/src/canciones.js
+++ music4you/src/canciones.js
@@ -1,44 +1,63 @@
 	 import Boney from './assets/IMG/Boney.png';
+	 import Donna from './assets/IMG/donna.png';
+	 import Duran from './assets/IMG/Duran.png';
+	 import Falco from "./assets/IMG/slider1.png";
+	 import Lady from "./assets/IMG/LG1.png";
+	 import Mick from "./assets/IMG/mick.jpg";
+	 import Queen from "./assets/IMG/Queen1.png";
+	 import Boney2 from "./assets/IMG/Boney.png";
+	 import Boney3 from "./assets/IMG/logo.png";
+
+	 //audio
+	 import Wild from "./assets/songs/wild.mp3";
+	 import love from "./assets/songs/donna.mp3";
+	 import Rio from "./assets/songs/rio.mp3";
+	 import commisser from "./assets/songs/falco.mp3";
+	 import Poker from  "./assets/songs/lg.mp3";
+	 import Streets from "./assets/songs/mick.mp3";
+	 import Radio from "./assets/songs/queen.mp3";
+	 import Rasputin from "./assets/songs/Raspu.mp3";
+	 import Babilon from "./assets/songs/rivers.mp3";
 	 
 
 	const canciones =  [ 
 		{
 			"id" : 1,
-			"nombre": "One night in Bangkok",
-			"ruta": "./assets/songs/Boney M",
-			"icono": Boney,
-			"artista": "Boney M",
+			"nombre": "Wild Boys",
+			"ruta": Wild,
+			"icono": Duran,
+			"artista": "Duran Duran",
 			"reproducciones": 15
 		},
 		{
 			"id" : 2,
 			"nombre": "I feel love",
-			"ruta": "./assets/songs/donna.mp3",
-			"icono": "./assets/IMG/Donna.png",
+			"ruta": love, 
+			"icono": Donna,
 			"artista": "Donna Summer",
 			"reproducciones": 17
 		},
 		{
 			"id" : 3,
 			"nombre": "Rio",
-			"ruta": "./assets/songs/Rio.mp3",
-			"icono": "./assets/IMG/Duran.jpg",
+			"ruta": Rio,
+			"icono": Duran,
 			"artista": "Duran Duran",
 			"reproducciones": 30
 		},
 		{
 			"id" : 4,
 			"nombre": "Der Kommiser",
-			"ruta": "./assets/songs/falco.mp3",
-			"icono": "./assets/IMG/slider1.png",
+			"ruta": commisser,
+			"icono": Falco,
 			"artista": "Falco",
 			"reproducciones": 18
 		},
 		{
 			"id": 5,
 			"nombre": "Poker face",
-			"ruta": "./assets/songs/lg.mp3",
-			"icono": "./assets/IMG/LG1.png",
+			"ruta":Poker,
+			"icono": Lady,
 			"artista": "Lady Gaga",
 			"reproducciones": 29
 
@@ -46,24 +65,24 @@
 		{
 			"id": 6,
 			"nombre": "Dancing in the streets",
-			"ruta": "./assets/songs/mick.mp3",
-			"icono": "./assets/IMG/mick.jpg",
+			"ruta": Streets,
+			"icono": Mick,
 			"artista": "Mick Jagger & David Bowie",
 			"reproducciones": 22
 		},
 		{
 			"id": 7,
 			"nombre": "Radio Ga-ga",
-			"ruta": "./assets/songs/queen.mp3",
-			"icono": "./assets/IMG/Queen1.png",
+			"ruta": Radio,
+			"icono": Queen,
 			"artista": "Queen",
 			"reproducciones": 23
 		},
 		{
 			"id": 8,
 			"nombre": "Rasputin",
-			"ruta": "./assets/songs/Raspu.mp3",
-			"icono": "./IMG/Boney.png",
+			"ruta": Rasputin,
+			"icono": Boney2,
 			"artista": "Boney M",
 			"reproducciones": 25
 		},
@@ -71,8 +90,8 @@
 		{
 			"id": 9,
 			"nombre":  "By the rivers of Babilon",
-			"ruta": "./assets/songs/rivers.mp3",
-			"icono": "./IMG/logo.png",
+			"ruta": Babilon,
+			"icono": Boney3,
 			"artista": "Boney M",
 			"reproducciones": 20
 		}
--- music4you/src/pages/Songs.jsx
+++ music4you/src/pages/Songs.jsx
@@ -1,46 +1,45 @@
 import React from "react";
-import { Container, Row, Col, Card, Image  } from "react-bootstrap";
+import {Card, Image } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
-import canciones from "../canciones";
+import canciones from "../canciones.js";
 import "./Songs.css";
-import Footer from "../common/Footer";
-
 
 const Songs = () => {
   return (
-    <div className="posting">
+    <div className="grid">
       {canciones.map((cancion, i) => {
         return (
-          <>
-          <Container>
-            <Row>
-              <Col lg={6}>
-                <Card>
-                  <Card.Header>
-                    <Image src={cancion.icono} variant="top" alt="" key={i} />
-                  </Card.Header>
+          
+          
+           
+              <Card style={{ width: "22rem" }} className="card">
+                <Card.Header className="card-image">
+                  <Image
+                    className="img"
+                    src={cancion.icono}
+                    variant="top"
+                    alt=""
+                    key={i}
+                  />
+                </Card.Header>
 
-                  <Card.Body>
-                    <Card.Title>
-                      <h3>{cancion.nombre}</h3>
-                    </Card.Title>
-                    <Card.Text>
-                      <h2>{cancion.artista}</h2>
-                    </Card.Text>
-                  </Card.Body>
-                  <Card.Footer>
-                    <audio controls src={cancion.ruta}></audio>
-                  </Card.Footer>
-                </Card>
-              </Col>
-            </Row>
-          </Container>
-          <Footer/>
-         </>
+                <Card.Body>
+                  <Card.Title>
+                    <h3>{cancion.nombre}</h3>
+                  </Card.Title>
+                  <Card.Text>
+                    <h2>{cancion.artista}</h2>
+                  </Card.Text>
+                </Card.Body>
+                <Card.Footer>
+                  <audio className="audio" controls src={cancion.ruta}></audio>
+                </Card.Footer>
+              </Card>
+              
           
         );
       })}
-    </div>
+      </div>
   );
 };
 
